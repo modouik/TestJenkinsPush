02a4cc4e8ca040175591f3b97b4ab3df961ea6e3
mohamedamine.douik <mohamedamine.douik@groupe-telnet.net>
2017-08-23 10:15:27 +0100

    test jenkins WITHOUT ERROR

--- app/src/main/java/com/madouik/testjenkinspush/MainActivity.java
+++ app/src/main/java/com/madouik/testjenkinspush/MainActivity.java
@@ -1,10 +1,13 @@
 package com.madouik.testjenkinspush;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+Object object = new Object();
+
+
+        Log.e(TAG, "onCreate: ", new Throwable() );
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
